d6cbdaffb67e18833a6e7a8b511baea405a73e15
Koji Matsumoto <otomustam.ijok@gmail.com>
2022-09-07 22:21:32 +0900

    fix reader-csv

--- src/reader/csv.rs
+++ src/reader/csv.rs
@@ -1,24 +1,32 @@
 use crate::cli::{
-    ArgRequired::{False, True},
-    CmdArg, CmdArgEntry, DefaultValue,
+    ArgRequired, CmdArg, CmdArgEntry, DefaultValue,
 };
-use crate::extractor::Extractor;
 use crate::util::get_schema;
 use arrow::{error::ArrowError, record_batch::RecordBatch};
+use arrow::csv;
+use arrow::datatypes::Schema;
 use clap::ArgMatches;
+use futures::stream::{iter, BoxStream};
+use std::io::Read;
 use std::sync::Arc;
 
+#[derive(Clone)]
 pub struct Reader {
-    batch_reader: Box<dyn Iterator<Item = Result<RecordBatch, ArrowError>>>,
+    batch_size: usize,
+    schema: Schema,
+    has_header: bool,
+    delimiter: Option<u8>,
+    bounds: Option<(usize, usize)>,
+    projection: Option<Vec<usize>>,
+    datetime_format: Option<String>,
 }
 
 impl Reader {
-    pub fn new(matches: &ArgMatches, extractor: Extractor) -> Self {
+    pub fn new(matches: &ArgMatches) -> Self {
         let batch_size: usize = matches.value_of_t("batch-size").unwrap();
-        let batch_extractor = extractor.batch_extractor();
         let schema_file_path = String::from(matches.value_of("schema-file").unwrap());
         let schema = get_schema(schema_file_path).unwrap();
-        let header = matches.is_present("input-csv-header");
+        let has_header = matches.is_present("input-csv-header");
         let delimiter = if let Some(d) = matches.value_of("input-csv-delimiter") {
             if d.len() == 1 {
                 Some(d.as_bytes()[0])
@@ -31,18 +39,15 @@ impl Reader {
         let bounds = None;
         let projection = None;
         let datetime_format = None;
-        let batch_reader = arrow::csv::reader::Reader::new(
-            batch_extractor,
-            Arc::new(schema),
-            header,
-            delimiter,
+
+        Self {
             batch_size,
+            schema,
+            has_header,
+            delimiter,
             bounds,
             projection,
             datetime_format,
-        );
-        Self {
-            batch_reader: Box::new(batch_reader),
         }
     }
 
@@ -53,37 +58,42 @@ impl Reader {
                 "Schema file (BigQuery schema)",
                 "schema",
                 true,
-                True,
+                ArgRequired::True,
             ),
             CmdArgEntry::new(
                 "batch-size",
                 "number of records in each files",
                 "batch-size",
                 true,
-                False(DefaultValue::String(String::from("10000"))),
+                ArgRequired::False(DefaultValue::String(String::from("10000"))),
             ),
             CmdArgEntry::new(
                 "input-csv-header",
                 "Input CSV has header",
                 "input-csv-header",
                 false,
-                False(DefaultValue::Bool(false)),
+                ArgRequired::False(DefaultValue::Bool(false)),
             ),
             CmdArgEntry::new(
                 "input-csv-delimiter",
                 "Delimiter of input CSV",
                 "input-csv-delimiter",
                 true,
-                False(DefaultValue::String(String::from(","))),
+                ArgRequired::False(DefaultValue::String(String::from(","))),
             ),
         ])
     }
-}
 
-impl Iterator for Reader {
-    type Item = RecordBatch;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.batch_reader.next().map(|batch_res| batch_res.unwrap())
+    pub fn stream(&self, file: Box<dyn Read + Send>) -> BoxStream<'static, Result<RecordBatch, ArrowError>> {
+        Box::pin(iter(csv::reader::Reader::new(
+            file,
+            Arc::new(self.schema.clone()),
+            self.has_header,
+            self.delimiter.clone(),
+            self.batch_size.clone(),
+            self.bounds.clone(),
+            self.projection.clone(),
+            self.datetime_format.clone(),
+        )))
     }
 }
