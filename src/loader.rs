73cfa7010adcbcd92db42c16a34b1eea4ae68437
Koji Matsumoto <otomustam.ijok@gmail.com>
2022-09-08 21:19:44 +0900

    fix module list for extractor and loader

--- src/loader.rs
+++ src/loader.rs
@@ -11,10 +11,5 @@ mod local;
 #[cfg(feature = "loader-local")]
 pub use local::Loader;
 
-#[cfg(feature = "loader-dummy")]
-mod dummy;
-#[cfg(feature = "loader-dummy")]
-pub use dummy::{cmd_args, get_future};
-
 #[cfg(not(feature = "loader"))]
 compile_error!("feature loader-* not enabled.");
